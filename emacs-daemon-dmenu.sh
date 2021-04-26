20c1ec002c47ffdbcabf664b4cc8eacb67e14037
Cycatz <thesummernightcat@gmail.com>
2021-04-26 09:00:29 +0800

    Remove <customize> and add <no-daemon> option

--- emacs-daemon-dmenu.sh
+++ emacs-daemon-dmenu.sh
@@ -40,7 +40,6 @@ start_client()
 {
 
     local SERVER_DIR="/run/user/1000/emacs/"
-    local DEFAULT_DAEMON_NAME="default"
     local DISPLAY_MODE="graphical" 
 
     usage()
@@ -49,8 +48,6 @@ start_client()
 Usage '$0 start':
 - -d <dir> or --server-dir <dir>
   - Specify the directory where Emacs server sockets live, default is '$SERVER_DIR'.
-- -n <name> or --daemon-name <name>
-  - Specify the default Emacs server daemon name, default is '$DEFAULT_DAEMON_NAME'.    
 - -t, -nw,  --tty
   - Create the client frame on the current terminal
 EOT
@@ -60,7 +57,6 @@ EOT
     while [[ $# -gt 0 ]]; do
         case "$1" in 
             -d|--server-dir   ) shift; SERVER_DIR="$1";;
-            -n|--default-name ) shift; DEFAULT_DAEMON_NAME="$1";;
             -t|-nw|--tty      ) DISPLAY_MODE="tty";;
             -h|--help         ) usage;;
             *                 ) usage;;  
@@ -70,23 +66,25 @@ EOT
 
     local -a sockets
     local socket_name
+    local cmd
 
     mapfile -t sockets < <(command ls -1 "$SERVER_DIR")
-    sockets+=("<customize>")
+    sockets+=("<no-daemon>")
 
     socket_name=$(printf '%s\n' "${sockets[@]}" | rofi -dmenu -p "Select the server")
 
-    if [[ "$socket_name" == "<customize>" ]]; then
-        socket_name=$(rofi -dmenu -p "Enter the daemon name")
-    fi
-
-    socket_path="${SERVER_DIR}${socket_name}"
-
-    if [[ "$DISPLAY_MODE" == "tty" ]]; then  
-        emacsclient -t -c --socket-name="$socket_path" -a ""
+    if [[ "$socket_name" == "<no-daemon>" ]]; then
+        cmd+="emacs"
     else
-        emacsclient -c --socket-name="$socket_path" -a ""
+        socket_path="${SERVER_DIR}${socket_name}"
+        cmd+="emacsclient -c -a '' --socket-name=$socket_path"
     fi
+
+    if [[ "$DISPLAY_MODE" == "tty" ]]; then
+        cmd+=" -t"
+    fi
+
+    eval "$cmd"
 }
 
 
